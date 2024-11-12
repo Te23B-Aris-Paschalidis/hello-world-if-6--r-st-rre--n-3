b6580163ff6c20684d0b42a427203a3f45e397cf
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-11-12 13:25:38 +0100

    wdagefgsav

--- 6 större än 3 övning/Program.cs
+++ 6 större än 3 övning/Program.cs
@@ -21,7 +21,7 @@
 //         Console.WriteLine("Välkommen");
 //     }
 //     else
-    //     {
+//     {
 //         Console.WriteLine("Fel namn eller lösenord");
 // //     }
 
@@ -40,11 +40,76 @@
 
 // Console.ReadLine();
 
-string pas = "";
-Console.WriteLine("skriv rätt lösenord");
-while (pas != "nopass")
+// string pas = "";
+// Console.WriteLine("skriv rätt lösenord");
+// while (pas != "nopass")
+// {
+//     pas = Console.ReadLine();
+// }
+
+// Console.ReadLine();
+
+// for (int kör = 0; kör < 5; kör++)
+// {
+//     Console.WriteLine("skriv ett nummer 1-5");
+//     string num = Console.ReadLine();
+//     int n = 0;
+//     int.TryParse(num, out n);
+//     if (n > 5)
+//     {
+//         Console.WriteLine("Högre en fem!");
+//     }
+//     else if (n < 5)
+//     {
+//         Console.WriteLine("Det är mindre en fem");
+//     }
+
+
+// }
+
+// Console.ReadLine();
+
+// int target = 0;
+// bool success = false;
+
+// while (success == false)
+// {
+//     Console.WriteLine("skriv ett tal");
+//     String num = Console.ReadLine();
+
+//     success = int.TryParse(num, out target);
+// }
+
+// Console.ReadLine();
+
+
+int tal = 0;
+
+while (tal != 7)
 {
-    pas = Console.ReadLine();
+    bool success = false;
+
+    while (success == false)
+    {
+        Console.WriteLine("Gissa rätt sifra mellan 1-10");
+        String num = Console.ReadLine();
+        success = int.TryParse(num, out tal);
+    }
+
+    if(tal < 7)
+    {
+        Console.WriteLine("Talet är större");
+    }
+    else if (tal > 7)
+    {
+        Console.WriteLine("Talet är mindre");
+    }
+    else
+    {
+    Console.WriteLine("Rätt svar");
+    }
+
 }
+
 
 Console.ReadLine();
