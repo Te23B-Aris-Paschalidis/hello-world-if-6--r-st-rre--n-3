3f6f7202934f022b09ed29a8ce44633d0cde5e06
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-11-12 12:24:18 +0100

    lvliug

---
+++ 6 större än 3 övning/Program.cs
@@ -0,0 +1,50 @@
+﻿// // // if (6 >= 3)
+// // {
+
+// // Console.WriteLine("Hello word");
+// // }
+
+
+// // // Console.ReadLine()
+// string lös;
+// Console.WriteLine("Hej vad heter du?");
+// string name = Console.ReadLine();
+
+// Console.WriteLine("Okej vad är ditt lösenord");
+// lös = Console.ReadLine();
+
+
+// if (name == "noname")
+// {
+//     if (lös == "nopass")
+//     {
+//         Console.WriteLine("Välkommen");
+//     }
+//     else
+    //     {
+//         Console.WriteLine("Fel namn eller lösenord");
+// //     }
+
+// // }
+
+
+
+
+// Console.ReadLine();
+
+
+// for (int wew = 0; wew <32; wew++)
+// {
+//     Console.WriteLine("Welcome world!");    
+// }
+
+// Console.ReadLine();
+
+string pas = "";
+Console.WriteLine("skriv rätt lösenord");
+while (pas != "nopass")
+{
+    pas = Console.ReadLine();
+}
+
+Console.ReadLine();
